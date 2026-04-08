01e9567a8eeacf25313ca696145c398b7be22771
nmueller18 <mueller-scheessel@gmx.net>
2026-04-09 00:13:47 +0200

    No testing of actual plots.

--- tests/testthat/test-age.comp.plot.R
+++ tests/testthat/test-age.comp.plot.R
@@ -23,21 +23,3 @@ test_that("age.comp.plot() throws an error when wrong mode is supplied", {
   expect_error(age.comp.plot(bay.ta_compare_diag, known_age = spitalfields$Age,
                                   mean_choice = "average"))
 })
-
-test_that("age.comp.plot() produces correct output", {
-  bay.ta_compare <- readRDS(test_path("fixtures", "spitalfields_res.Rds"))
-  bay.ta_compare_diag <- diagnostic.summary(bay.ta_compare)
-  vdiffr::expect_doppelganger("spitalfields_age_comparison",
-                              suppressWarnings(age.comp.plot(bay.ta_compare_diag,
-                                    known_age = spitalfields$Age)))
-})
-
-test_that("age.comp.plot() produces correct output wit reduced dataset", {
-  bay.ta_compare <- readRDS(test_path("fixtures", "spitalfields_res.Rds"))
-  bay.ta_compare_diag <- diagnostic.summary(bay.ta_compare)
-  spitalfields_Age <- spitalfields$Age
-  spitalfields_Age[c(10,32,56,110,138)] <- NA
-  vdiffr::expect_doppelganger("spitalfields_age_comparison_missing_data",
-                              suppressWarnings(age.comp.plot(bay.ta_compare_diag,
-                                    known_age = spitalfields_Age)))
-})
